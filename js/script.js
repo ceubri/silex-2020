0a06bf56e89d15b8fc52f027f003450818302627
ceubri <ceubrak@yahoo.fr>
2020-05-14 10:09:36 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+var wrapper = document.querySelector('.wrapper svg')
+var btnDraw = document.querySelector('.btn-draw')
+var btnErase = document.querySelector('.btn-erase')
+
+// We are only adding and removing the 'active' class,
+// the entire animation is defined in the CSS code
+function draw() {
+  wrapper.classList.add('active')
+}
+
+function erase() {
+  wrapper.classList.remove('active')
+}
+
+// Add handlers to our buttons
+btnDraw.addEventListener('click', draw, false)
+btnErase.addEventListener('click', erase, false)
+
+// Play draw animation once
+setTimeout(draw, 300)
